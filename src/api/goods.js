b09b6ad85bac6fd60f6457c58c7785454e922ad2
yaofeng <2997310192@qq.com>
2019-08-26 20:30:15 +0800

    路由更新

--- src/api/goods.js
+++ src/api/goods.js
@@ -6,6 +6,12 @@ let APIgoods = {
     },
     getSecondCategories(id){
         return axios.get("xhr/list/subCate.json?categoryId="+id).then(res => res.data)
+    },
+    getCategoryGoods(id1,id2){
+        return axios.get(`/xhr/list/l2Items2.json?categoryL1Id=${id1}&categoryL2Id=${id2}`).then(res => res.data)
+    },
+    getGoodsDetails(goodsId){
+        return axios.get("/xhr/item/detail.json?itemId="+goodsId).then(res => res.data)
     }
 }
 export default APIgoods
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from './view/HomePage.vue'
+import IndexNav from './view/IndexNav.vue'
 import ClassifyPage from './view/ClassifyPage.vue'
 import CartPage from './view/CartPage.vue'
 import UserPage from './view/UserPage.vue'
@@ -8,9 +9,12 @@ import UserPage from './view/UserPage.vue'
 Vue.use(Router)
 export default new Router({
   routes: [
-    {path: "/", component: HomePage},
-    {path: "/ClassifyPage", component: ClassifyPage},
-    {path: "/CartPage", component: CartPage},
-    {path: "/UserPage", component: UserPage},
+    {path: "/", component: IndexNav,children:[
+      {path: "", component: HomePage},//将HomePage设置为默认页面
+      {path: "HomePage", component: HomePage},
+      {path: "ClassifyPage", component: ClassifyPage},
+      {path: "CartPage", component: CartPage},
+      {path: "UserPage", component: UserPage},
+    ]},
   ]
 })
